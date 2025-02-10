adadc3dd299de6bd2c4e3429f722024cecfebe82
Jesus Rodriguez <jrodsan0407@g.educaand.es>
2025-02-10 15:59:55 +0100

    Comprobar que la contraseña es segura

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -11,10 +11,25 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   String? _emailError;
+  String? _passwordError;
 
   void _validateEmail() {
     setState(() {
       _emailError = _emailController.text.contains('@') ? null : 'El correo debe contener "@"';
+    });
+  }
+
+  void _validatePassword() {
+    final password = _passwordController.text;
+    final bool hasUppercase = password.contains(RegExp(r'[A-Z]'));
+    final bool hasLowercase = password.contains(RegExp(r'[a-z]'));
+    final bool hasDigit = password.contains(RegExp(r'\d'));
+    final bool hasMinLength = password.length >= 8;
+
+    setState(() {
+      _passwordError = (hasUppercase && hasLowercase && hasDigit && hasMinLength)
+          ? null
+          : 'La contraseña debe incluir:\n- Min 1 mayúscula\n- Min 1 minúscula\n- Min 1 número\n- Min 8 caracteres';
     });
   }
 
@@ -39,8 +54,12 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 10),
             TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Contraseña'),
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+                errorText: _passwordError,
+              ),
               obscureText: true,
+              onChanged: (_) => _validatePassword(),
             ),
           ],
         ),
