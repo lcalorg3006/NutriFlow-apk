a077cf026983479027772bcaed91fbc886202ee9
Jesus Rodriguez <jrodsan0407@g.educaand.es>
2025-02-10 17:05:47 +0100

    Cambios estilos

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green, secondary: Colors.greenAccent),
       ),
       home: const LoginPage(),
     );
