5ce7d6282223685f7d14d0b7eff4e44d9120d261
Jesus Rodriguez <jrodsan0407@g.educaand.es>
2025-02-10 15:53:35 +0100

    Añadir assets y crear campos de formulario usuario/contraseña

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nutriflow_app/screens/forgot_password_page.dart';
+import 'package:nutriflow_app/screens/login_page.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ForgotPasswordPage(),
+      home: const LoginPage(),
     );
   }
 }
+
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Correo electrónico'),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Contraseña'),
+              obscureText: true,
+            ),
+          ],
         ),
       ),
     );
